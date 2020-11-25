71c8b99207a8b73ebf133ef62d4c0db7d1b406cc
Arivas15 <54857672+Arivas15@users.noreply.github.com>
2020-11-25 15:28:30 -0600

    Porgress1

---
+++ ThirdPersonBase-main/ThirdPerson/Assets/CameraPortal.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPortal : MonoBehaviour
+{
+    public Transform playerCamera;
+    public Transform portal;
+    public Transform portal2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerOffsetFromPortal = playerCamera.position - portal2.position;
+        transform.position = portal.position + playerOffsetFromPortal;
+
+        float angularDifferenceBetweenRotation = Quaternion.Angle(portal.rotation, portal2.rotation);
+        Quaternion portalRotationDifference = Quaternion.AngleAxis(angularDifferenceBetweenRotation, Vector3.up);
+        Vector3 newCameraDirection = portalRotationDifference * playerCamera.forward;
+        transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
+    }
+}
